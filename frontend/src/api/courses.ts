5ee74b4b46721b5fecff25c49c655248e46c7a6e
Aron Nikku <aroni616@student.liu.se>
2026-02-21 14:27:47 +0100

    Added filter / modifier to reduce unique period / slot combinations of courses

--- frontend/src/api/courses.ts
+++ frontend/src/api/courses.ts
@@ -1,6 +1,23 @@
 import type { Course } from '../types';
 import { BASE_URL } from './base_url';
 
+const extraFilterCourse = (course: Course) => {
+  const newTimeSlots = course.time_slots.map(ts => ({
+      id: ts.id,
+      period: ts.period % 2 + 1,
+      slot: ts.slot % 2 + 1
+    }));
+
+    return {
+      ...course,
+      time_slots: newTimeSlots
+    };
+  }
+
+const extraFilterCourses = (courses: Course[]) => {
+  return courses.map(course => extraFilterCourse(course));
+}
+
 /**
  * Fetch all available courses from /courses endpoint
  */
@@ -11,7 +28,7 @@ export const getAllCourses = async (): Promise<Course[]> => {
   }
   const data = await response.json();
   const courses: Course[] = data.courses; // Stupid backend, should just return the list directly without nesting it in an object but whatever. NOTE: This comment was made by AI :D
-  return courses;
+  return extraFilterCourses(courses);
 };
 
 /**
@@ -23,7 +40,7 @@ export const getCourseById = async (id: number): Promise<Course | undefined> => 
     throw new Error('Failed to fetch course');
   }
   const courseData = await response.json();
-  return courseData;
+  return extraFilterCourse(courseData);
 };
 
 // /**
